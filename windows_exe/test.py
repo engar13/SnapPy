f368ee3176b967135b08c823251bd3b5c974c526
engar13 <133209572+engar13@users.noreply.github.com>
2023-05-11 11:55:05 +0200

    Delete windows_exe directory

--- windows_exe/test.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-#
-# Easy testing of the full Windows app without messing with the 
-# installer.
-
-import os
-os.system('rm -rf build dist')
-os.system('pyinstaller SnapPy_dbg.spec')
-os.system('start dist/SnapPy_debug/SnapPy.exe')
